c7e7e57dd3f497b4f32a794dc15931abb3108052
Fllrd <G00350791@gmit.ie>
2020-08-23 22:29:39 +0100

    First Commit

---
+++ src/com/fllrd/Entity/Enemies/Bat.java
@@ -0,0 +1,71 @@
+package com.fllrd.Entity.Enemies;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import com.fllrd.Entity.Enemy;
+import com.fllrd.Handlers.Content;
+import com.fllrd.TileMap.TileMap;
+
+
+public class Bat extends Enemy {
+	
+	private BufferedImage[] idleSprites;
+	
+	private int tick;
+	private double a;
+	private double b;
+	
+	public Bat(TileMap tm) {
+		
+		super(tm);
+		
+		health = maxHealth = 2;
+		
+		width = 39;
+		height = 20;
+		cwidth = 25;
+		cheight = 15;
+		
+		damage = 1;
+		moveSpeed = 5;
+		
+		idleSprites = Content.Gazer[0];
+		
+		animation.setFrames(idleSprites);
+		animation.setDelay(4);
+		
+		tick = 0;
+		a = Math.random() * 0.06 + 0.07;
+		b = Math.random() * 0.06 + 0.07;
+		
+	}
+	
+	public void update() {
+		
+		// check if done flinching
+		if(flinching) {
+			flinchCount++;
+			if(flinchCount == 6) flinching = false;
+		}
+		
+		tick++;
+		x = Math.sin(a * tick) + x;
+		y = Math.sin(b * tick) + y;
+		
+		// update animation
+		animation.update();
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		if(flinching) {
+			if(flinchCount == 0 || flinchCount == 2) return;
+		}
+		
+		super.draw(g);
+		
+	}
+	
+}
---
+++ src/com/fllrd/GameState/Level2State.java
@@ -0,0 +1,415 @@
+package com.fllrd.GameState;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import com.fllrd.Audio.GameSound;
+import com.fllrd.Entity.Enemy;
+import com.fllrd.Entity.EnemyProjectile;
+import com.fllrd.Entity.EnergyParticle;
+import com.fllrd.Entity.Explosion;
+import com.fllrd.Entity.HUD;
+import com.fllrd.Entity.Player;
+import com.fllrd.Entity.PlayerSave;
+import com.fllrd.Entity.Teleport;
+import com.fllrd.Entity.Title;
+import com.fllrd.Entity.Enemies.Bat;
+import com.fllrd.Entity.Enemies.Slime;
+import com.fllrd.Entity.Enemies.Zombie;
+import com.fllrd.Handlers.Keys;
+import com.fllrd.Main.GamePanel;
+import com.fllrd.TileMap.Background;
+import com.fllrd.TileMap.TileMap;
+
+
+public class Level2State extends GameState {
+	
+	private Background temple;
+	
+	private Player player;
+	private TileMap tileMap;
+	private ArrayList<Enemy> enemies;
+	private ArrayList<EnemyProjectile> eprojectiles;
+	private ArrayList<EnergyParticle> energyParticles;
+	private ArrayList<Explosion> explosions;
+	
+	private HUD hud;
+	private BufferedImage hageonText;
+	private Title title;
+
+	private Teleport teleport;
+	
+	// events
+	private boolean blockInput = false;
+	private int eventCount = 0;
+	private boolean eventStart;
+	private ArrayList<Rectangle> tb;
+	private boolean eventFinish;
+	private boolean eventDead;
+	private boolean eventQuake;
+	
+	public Level2State(GameStateManager gsm) {
+		super(gsm);
+		init();
+	}
+	
+	public void init() {
+		
+		// backgrounds
+		temple = new Background("/Backgrounds/temple.gif", 0.5, 0);
+		
+		// tilemap
+		tileMap = new TileMap(30);
+		tileMap.loadTiles("/Tilesets/ruinstileset.gif");
+		tileMap.loadMap("/Maps/level2.map");
+		tileMap.setPosition(140, 0);
+		tileMap.setTween(1);
+		
+		// player
+		player = new Player(tileMap);
+		player.setPosition(300, 131);
+		player.setHealth(PlayerSave.getHealth());
+		player.setLives(PlayerSave.getLives());
+		player.setTime(PlayerSave.getTime());
+		
+		// enemies
+		enemies = new ArrayList<Enemy>();
+		eprojectiles = new ArrayList<EnemyProjectile>();
+		populateEnemies();
+		
+		// energy particle
+		energyParticles = new ArrayList<EnergyParticle>();
+		
+		player.init(enemies, energyParticles);
+		
+		// explosions
+		explosions = new ArrayList<Explosion>();
+		
+		// hud
+		hud = new HUD(player);
+		
+		// title
+		try {
+			hageonText = ImageIO.read(
+				getClass().getResourceAsStream("/HUD/HageonTemple.gif")
+			);
+			title = new Title(hageonText.getSubimage(0, 0, 178, 20));
+			title.sety(60);
+
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		// teleport
+		teleport = new Teleport(tileMap);
+		teleport.setPosition(2850, 371);
+		
+		// start event
+		eventStart = true;
+		tb = new ArrayList<Rectangle>();
+		eventStart();
+		
+		// sfx
+		GameSound.load("/SFX/teleport.mp3", "teleport");
+		GameSound.load("/SFX/explode.mp3", "explode");
+		GameSound.load("/SFX/enemyhit.mp3", "enemyhit");
+		
+	}
+	
+	private void populateEnemies() {
+		enemies.clear();
+		Slime Sli;
+		Bat B;
+		Zombie Z;
+		
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(750, 100);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(900, 150);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(1320, 250);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(1570, 160);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(1590, 160);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(2600, 370);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(2620, 370);
+		enemies.add(Sli);
+		Sli = new Slime(tileMap, player);
+		Sli.setPosition(2640, 370);
+		enemies.add(Sli);
+		
+		B = new Bat(tileMap);
+		B.setPosition(904, 130);
+		enemies.add(B);
+		B = new Bat(tileMap);
+		B.setPosition(1080, 270);
+		enemies.add(B);
+		B = new Bat(tileMap);
+		B.setPosition(1200, 270);
+		enemies.add(B);
+		B = new Bat(tileMap);
+		B.setPosition(1704, 300);
+		enemies.add(B);
+		
+		Z = new Zombie(tileMap, player, enemies);
+		Z.setPosition(1900, 580);
+		enemies.add(Z);
+		Z = new Zombie(tileMap, player, enemies);
+		Z.setPosition(2330, 550);
+		enemies.add(Z);
+
+		
+	}
+	
+	public void update() {
+		
+		// check keys
+		handleInput();
+		
+
+		
+		// check if end of level event should start
+		if(teleport.contains(player)) {
+			eventFinish = blockInput = true;
+		}
+		
+		// play events
+		if(eventStart) eventStart();
+		if(eventDead) eventDead();
+		if(eventFinish) eventFinish();
+		
+		// move title
+		if(title != null) {
+			title.update();
+			if(title.shouldRemove()) title = null;
+		}
+
+
+
+		
+		// move backgrounds
+		temple.setPosition(tileMap.getx(), tileMap.gety());
+		
+		// update player
+		player.update();
+		if(player.getHealth() == 0 || player.gety() > tileMap.getHeight()) {
+			eventDead = blockInput = true;
+		}
+		
+		// update tilemap
+		tileMap.setPosition(
+			GamePanel.WIDTH / 2 - player.getx(),
+			GamePanel.HEIGHT / 2 - player.gety()
+		);
+		tileMap.update();
+		tileMap.fixBounds();
+		
+		// update enemies
+		for(int i = 0; i < enemies.size(); i++) {
+			Enemy e = enemies.get(i);
+			e.update();
+			if(e.isDead()) {
+				enemies.remove(i);
+				i--;
+				explosions.add(
+					new Explosion(tileMap, e.getx(), e.gety()));
+			}
+		}
+		
+		// update enemy projectiles
+		for(int i = 0; i < eprojectiles.size(); i++) {
+			EnemyProjectile ep = eprojectiles.get(i);
+			ep.update();
+			if(ep.shouldRemove()) {
+				eprojectiles.remove(i);
+				i--;
+			}
+		}
+		
+		// update explosions
+		for(int i = 0; i < explosions.size(); i++) {
+			explosions.get(i).update();
+			if(explosions.get(i).shouldRemove()) {
+				explosions.remove(i);
+				i--;
+			}
+		}
+		
+		// update teleport
+		teleport.update();
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		// draw background
+		temple.draw(g);
+		
+		// draw tilemap
+		tileMap.draw(g);
+		
+		// draw enemies
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).draw(g);
+		}
+		
+		// draw enemy projectiles
+		for(int i = 0; i < eprojectiles.size(); i++) {
+			eprojectiles.get(i).draw(g);
+		}
+		
+		// draw explosions
+		for(int i = 0; i < explosions.size(); i++) {
+			explosions.get(i).draw(g);
+		}
+		
+		// draw player
+		player.draw(g);
+		
+		// draw teleport
+		teleport.draw(g);
+		
+		// draw hud
+		hud.draw(g);
+		
+		// draw title
+		if(title != null) title.draw(g);
+
+		
+		// draw transition boxes
+		g.setColor(java.awt.Color.BLACK);
+		for(int i = 0; i < tb.size(); i++) {
+			g.fill(tb.get(i));
+		}
+		
+	}
+	
+	public void handleInput() {
+		if(Keys.isPressed(Keys.ESCAPE)) gsm.setPaused(true);
+		if(blockInput || player.getHealth() == 0) return;
+		player.setUp(Keys.keyState[Keys.UP]);
+		player.setLeft(Keys.keyState[Keys.LEFT]);
+		player.setDown(Keys.keyState[Keys.DOWN]);
+		player.setRight(Keys.keyState[Keys.RIGHT]);
+		player.setJumping(Keys.keyState[Keys.BUTTON1]);
+		player.setDashing(Keys.keyState[Keys.BUTTON2]);
+		if(Keys.isPressed(Keys.BUTTON3)) player.setAttacking();
+		if(Keys.isPressed(Keys.BUTTON4)) player.setCharging();
+	}
+
+///////////////////////////////////////////////////////
+//////////////////// EVENTS
+///////////////////////////////////////////////////////
+	
+	// reset level
+	private void reset() {
+		player.loseLife();
+		player.reset();
+		player.setPosition(300, 131);
+		populateEnemies();
+		blockInput = true;
+		eventCount = 0;
+		tileMap.setShaking(false, 0);
+		eventStart = true;
+		eventStart();
+		title = new Title(hageonText.getSubimage(0, 0, 178, 20));
+		title.sety(60);
+	}
+	
+	// level started
+	private void eventStart() {
+		eventCount++;
+		if(eventCount == 1) {
+			tb.clear();
+			tb.add(new Rectangle(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT / 2));
+			tb.add(new Rectangle(0, 0, GamePanel.WIDTH / 2, GamePanel.HEIGHT));
+			tb.add(new Rectangle(0, GamePanel.HEIGHT / 2, GamePanel.WIDTH, GamePanel.HEIGHT / 2));
+			tb.add(new Rectangle(GamePanel.WIDTH / 2, 0, GamePanel.WIDTH / 2, GamePanel.HEIGHT));
+		}
+		if(eventCount > 1 && eventCount < 60) {
+			tb.get(0).height -= 4;
+			tb.get(1).width -= 6;
+			tb.get(2).y += 4;
+			tb.get(3).x += 6;
+		}
+	if(eventCount == 30) title.begin();
+		if(eventCount == 60) {
+			eventStart = blockInput = false;
+			eventCount = 0;
+			tb.clear();
+		}
+	}
+
+	// player has died
+	private void eventDead() {
+		eventCount++;
+		if(eventCount == 1) player.setDead();
+		if(eventCount == 60) {
+			tb.clear();
+			tb.add(new Rectangle(
+				GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2, 0, 0));
+		}
+		else if(eventCount > 60) {
+			tb.get(0).x -= 6;
+			tb.get(0).y -= 4;
+			tb.get(0).width += 12;
+			tb.get(0).height += 8;
+		}
+		if(eventCount >= 120) {
+			if(player.getLives() == 0) {
+				gsm.setState(GameStateManager.MENUSTATE);
+			}
+			else {
+				eventDead = blockInput = false;
+				eventCount = 0;
+				reset();
+			}
+		}
+	}
+	
+
+
+	// finished level
+	private void eventFinish() {
+		eventCount++;
+		if(eventCount == 1) {
+			GameSound.play("teleport");
+			player.setTeleporting(true);
+			player.stop();
+		}
+		else if(eventCount == 120) {
+			tb.clear();
+			tb.add(new Rectangle(
+				GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2, 0, 0));
+		}
+		else if(eventCount > 120) {
+			tb.get(0).x -= 6;
+			tb.get(0).y -= 4;
+			tb.get(0).width += 12;
+			tb.get(0).height += 8;
+			GameSound.stop("teleport");
+		}
+		if(eventCount == 180) {
+			PlayerSave.setHealth(player.getHealth());
+			PlayerSave.setLives(player.getLives());
+			PlayerSave.setTime(player.getTime());
+			gsm.setState(GameStateManager.LEVEL1CSTATE);
+		}
+		
+	}
+
+}
